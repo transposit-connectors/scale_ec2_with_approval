9aa3f354ea4172b4c90220b8a1e8261402b4e6fe
Dan Moore <mooreds@transposit.com>
2019-11-14 22:51:41 +0000

    debugging

--- src/webhook.js
+++ src/webhook.js
@@ -7,7 +7,11 @@
  */
 ({ http_event }) => {
   const parsed_body = http_event.parsed_body;
-  console.log(JSON.parse(parsed_body.payload));
+  if (parsed_body.payload) {
+    console.log(JSON.parse(parsed_body.payload));
+  } else {
+    console.log(parsed_body);
+  }
   const workspaceId = parsed_body.team_id;
   const userId = parsed_body.user_id;
   const _ = require('underscore.js');
