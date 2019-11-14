158f49c44481d8c37f2a79d9cb91e513aa0f6bd1
Dan Moore <github@mooreds.com>
2019-11-14 16:22:57 -0700

    beautified

--- src/webhook.js
+++ src/webhook.js
@@ -18,16 +18,18 @@
     const userId = parsed_body.user_id;
     const _ = require('underscore.js');
     const types = ['t2.nano', 't2.micro', 't2.small', 't2.medium', 't2.large', 't2.xlarge', 't2.2xlarge'];
-    const options = types.map(t=>{
-      return { text: {         "type": "plain_text",
-                                "text": t,
-                      "emoji" :false
-                     },
-              value: t
-             };
+    const options = types.map(t => {
+        return {
+            text: {
+                "type": "plain_text",
+                "text": t,
+                "emoji": false
+            },
+            value: t
+        };
     });
 
-  console.log(options);
+    console.log(options);
     setImmediate(() => {
         let user = api.user({
             type: "slack",
