eacbf9c5dadd7de0bcba04949893c8f06a2a0174
Dan Moore <mooreds@transposit.com>
2019-11-15 21:35:00 +0000

    broke out parameters

---
+++ src/create_parameters_for_list_instances.js
@@ -0,0 +1,32 @@
+(params) => {
+  
+    const parameters = {};
+
+    parameters.channel = channel;
+    const one_section = {
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "XXX REPLACE ME"
+      },
+
+    };
+    parameters.blocks = [{
+      "type": "section",
+      "text": {
+        "type": "mrkdwn",
+        "text": "Instances in your account"
+      }
+    }];
+    const instances = api.run("this.describe_instances", {}, {
+      asUser: user.id
+    });
+    // let text = "";
+    instances.forEach(i => {
+      const obj = _.clone(one_section);
+      obj.text.text = i.id + " - " + i.type + " - " + i.state;
+      parameters.blocks.push(obj);
+    });
+  
+  return parameters;
+}
--- src/webhook.js
+++ src/webhook.js
@@ -72,33 +72,6 @@
     let command_text = "";
 
 
-    const parameters = {};
-
-    parameters.channel = channel;
-    const one_section = {
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "XXX REPLACE ME"
-      },
-
-    };
-    parameters.blocks = [{
-      "type": "section",
-      "text": {
-        "type": "mrkdwn",
-        "text": "Instances in your account"
-      }
-    }];
-    const instances = api.run("this.describe_instances", {}, {
-      asUser: user.id
-    });
-    // let text = "";
-    instances.forEach(i => {
-      const obj = _.clone(one_section);
-      obj.text.text = i.id + " - " + i.type + " - " + i.state;
-      parameters.blocks.push(obj);
-    });
 
     return api.run("this.post_chat_message", parameters);
 
