58c719d310b2b3c648b3490c3137e3f45540a2e4
Dan Moore <mooreds@transposit.com>
2019-11-15 18:46:42 +0000

    raw command parsing

--- src/webhook.js
+++ src/webhook.js
@@ -41,9 +41,9 @@
       console.log("here2");
       console.log(body.event.text);
       const raw_full_command = body.event.text;
-      const raw_command = raw_full_command.substr(raw_full_command.indexOf(" ") + 1);
+      const raw_command = raw_full_command.substr(raw_full_command.indexOf(">") + 2); // get rid of botusername
       const help_text = `I don't understand the command. Please either "list-ec2-instances" or `;
-      console.log(raw_command);
+      console.log("rc");console.log(raw_command);
 
       if (! raw_command ){
          return api.run("slack.post_chat_message", {text: help_text, channel: 'test5'});
