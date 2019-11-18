913e663faafca0ec5612ff1629c2d6b6fe09bab4
Dan Moore <github@mooreds.com>
2019-11-18 12:16:34 -0700

    Revert "Commit from Transposit IDE"
    This reverts commit c8c95578ae12ae152116937b1a1ee3e9d9ef8461.

---
+++ src/create_parameters_for_list_instances.js
@@ -0,0 +1,35 @@
+(params) => {
+  const _ = require('underscore.js');
+
+  const parameters = {};
+
+
+  parameters.channel = params.channel;
+  const user = params.user;
+  const one_section = {
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "XXX REPLACE ME"
+    },
+
+  };
+  parameters.blocks = [{
+    "type": "section",
+    "text": {
+      "type": "mrkdwn",
+      "text": "Instances in your account"
+    }
+  }];
+  const instances = api.run("this.describe_instances", {}, {
+    asUser: user.id
+  });
+  // let text = "";
+  instances.forEach(i => {
+    const obj = _.clone(one_section);
+    obj.text.text = i.id + " - " + i.type + " - " + i.state;
+    parameters.blocks.push(obj);
+  });
+
+  return parameters;
+}
