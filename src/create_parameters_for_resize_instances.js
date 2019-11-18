0f68629cbbc0cf8bda3f4c9fdc4c7c6f9d1bef5a
Dan Moore <mooreds@transposit.com>
2019-11-18 19:11:55 +0000

    set up blocks

--- src/create_parameters_for_resize_instances.js
+++ src/create_parameters_for_resize_instances.js
@@ -5,6 +5,7 @@
 
   parameters.channel = params.channel;
   const user = params.user;
+  parameters.blocks = [];
 
   const types = ['t2.nano', 't2.micro', 't2.small', 't2.medium', 't2.large', 't2.xlarge', 't2.2xlarge'];
   const options = types.map(t => {
