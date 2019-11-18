f25418d0aae9f0f6d56ddc221f71faa196f65fac
Dan Moore <mooreds@transposit.com>
2019-11-18 19:18:58 +0000

    logging

--- src/respond_to_resize.js
+++ src/respond_to_resize.js
@@ -1,5 +1,7 @@
 ({ http_event }) => {
   console.log(http_event);
+  console.log(http_event.parsed_body.payload);
+  console.log(JSON.parse(http_event.parsed_body.payload));
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
