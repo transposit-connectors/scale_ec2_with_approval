10474105893fc3f799a7677bb8d8cb5ff0c025ef
Dan Moore <mooreds@transposit.com>
2019-11-18 19:18:09 +0000

    logging

--- src/respond_to_resize.js
+++ src/respond_to_resize.js
@@ -1,12 +1,8 @@
 ({ http_event }) => {
+  console.log(http_event);
   return {
     status_code: 200,
     headers: { "Content-Type": "application/json" },
     body: { "greeting": "Hello World" }
   };
 }
-
-/*
- * For sample code and reference material, visit
- * https://www.transposit.com/docs/building/webhooks
- */
