b759f0a313d04b676328748fce4e82b99759c719
Dan Moore <github@mooreds.com>
2019-11-18 06:40:26 -0700

    beautifying

--- src/get_slack_message.js
+++ src/get_slack_message.js
@@ -5,15 +5,17 @@
  * For sample code and reference material, visit
  * https://www.transposit.com/docs/references/js-operations
  */
-({user}) => {
+({
+  user
+}) => {
   return {
     // The blocks get displayed in the actual message. 
     // You can play with block kit here: https://api.slack.com/tools/block-kit-builder
     blocks: [{
       "type": "section",
       "text": {
-          "type": "mrkdwn",
-          "text": "Hello World!"
+        "type": "mrkdwn",
+        "text": "Hello World!"
       }
     }],
     // The text content gets displayed in the notification
--- src/webhook.js
+++ src/webhook.js
@@ -28,74 +28,80 @@
 
   setImmediate(() => {
 
-    let user = api.user({
-      type: "slack",
-      workspaceId,
-      userId
-    });
-
-    if (!user) {
-      let text = `Configure user settings at ${env.getBuiltin().appUrl}`;
-      return api.run("this.post_text_only_message", {
-        text: help_text,
-        channel: channel
-      })
-    }
-
-    const raw_full_command = parsed_body.event.text;
-    const raw_command = raw_full_command.substr(raw_full_command.indexOf(">") + 2); // get rid of botusername
-    const help_text = "I don't understand the command. \n\nPlease either 'list-ec2-instances' or 'resize-ec2-instance \"instance-id\", approver: @name'";
-
-    // return api.run("slack.post_chat_message", {text: "abcd", channel: channel});
-    if (!raw_command) {
-      console.log("didn't see raw command");
-      return api.run("this.post_text_only_message", {
-        text: help_text,
-        channel: channel
+      let user = api.user({
+        type: "slack",
+        workspaceId,
+        userId
       });
-    }
-    if (raw_command.length < 1) {
-      console.log("didn't see raw command length");
-      return api.run("this.post_text_only_message", {
-        text: help_text,
-        channel: channel
-      });
-    }
-    
-    const list_cmd = new RegExp(/list-ec2-instances/).exec(raw_command) != null;
-    const resize_cmd = new RegExp(/resize-ec2-instances/).exec(raw_command) != null;
-    if (!(list_cmd || resize_cmd)) {
-      console.log("didn't see raw command we understood");
-      console.log(resize_cmd);
-      console.log(raw_command);
-      return api.run("this.post_text_only_message", {
-        text: help_text,
-        channel: channel
-      });
-    }
 
-    if (list_cmd) {
-      const parameters = api.run("this.create_parameters_for_list_instances",{channel: channel, user: user})[0];
-      return api.run("this.post_chat_message", parameters);
-    }
-    if (resize_cmd) {
-      const command_array = raw_command.split(/ +/);  
-       if (command_array.length != 4) {
-      console.log("didn't see resize command we understood");
-         console.log(command_array);
-         console.log(raw_command);
-      return api.run("this.post_text_only_message", {
-        text: help_text + " [saw "+raw_command+"]",
-        channel: channel
-      });
-    }
-      const parameters = api.run("this.create_parameters_for_resize_instances",{channel: channel, user: user})[0];
-      return api.run("this.post_chat_message", parameters);
-      
-       
+      if (!user) {
+        let text = `Configure user settings at ${env.getBuiltin().appUrl}`;
+        return api.run("this.post_text_only_message", {
+          text: help_text,
+          channel: channel
+        })
+      }
 
-  });
-  return {
-    status_code: 200
-  };
-}
+      const raw_full_command = parsed_body.event.text;
+      const raw_command = raw_full_command.substr(raw_full_command.indexOf(">") + 2); // get rid of botusername
+      const help_text = "I don't understand the command. \n\nPlease either 'list-ec2-instances' or 'resize-ec2-instance \"instance-id\", approver: @name'";
+
+      // return api.run("slack.post_chat_message", {text: "abcd", channel: channel});
+      if (!raw_command) {
+        console.log("didn't see raw command");
+        return api.run("this.post_text_only_message", {
+          text: help_text,
+          channel: channel
+        });
+      }
+      if (raw_command.length < 1) {
+        console.log("didn't see raw command length");
+        return api.run("this.post_text_only_message", {
+          text: help_text,
+          channel: channel
+        });
+      }
+
+      const list_cmd = new RegExp(/list-ec2-instances/).exec(raw_command) != null;
+      const resize_cmd = new RegExp(/resize-ec2-instances/).exec(raw_command) != null;
+      if (!(list_cmd || resize_cmd)) {
+        console.log("didn't see raw command we understood");
+        console.log(resize_cmd);
+        console.log(raw_command);
+        return api.run("this.post_text_only_message", {
+          text: help_text,
+          channel: channel
+        });
+      }
+
+      if (list_cmd) {
+        const parameters = api.run("this.create_parameters_for_list_instances", {
+          channel: channel,
+          user: user
+        })[0];
+        return api.run("this.post_chat_message", parameters);
+      }
+      if (resize_cmd) {
+        const command_array = raw_command.split(/ +/);
+        if (command_array.length != 4) {
+          console.log("didn't see resize command we understood");
+          console.log(command_array);
+          console.log(raw_command);
+          return api.run("this.post_text_only_message", {
+            text: help_text + " [saw " + raw_command + "]",
+            channel: channel
+          });
+        }
+        const parameters = api.run("this.create_parameters_for_resize_instances", {
+          channel: channel,
+          user: user
+        })[0];
+        return api.run("this.post_chat_message", parameters);
+
+
+
+      });
+    return {
+      status_code: 200
+    };
+  }
