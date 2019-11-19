9902b032cb938b8721565ce4a09ce8dd2019e7ad
Dan Moore <mooreds@transposit.com>
2019-11-19 23:29:08 +0000

    hand crafted restarting

--- src/resize_ec2_instance.js
+++ src/resize_ec2_instance.js
@@ -1,4 +1,17 @@
 (params) => {
+  
+  // let result = api.query("SELECT * FROM aws_ec2.stop_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  // let result = api.query("SELECT * FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  // const modifySql = "SELECT * FROM aws_ec2.modify_instance_attribute WHERE $body=(SELECT { 'Attribute' : 'instanceType', 'InstanceId' : '"+
+  //                        params.instanceId+"', 'InstanceType' : { 'Value' : '"+params.newSize+"'} })"
+  // console.log(modifySql);
+  // let result = api.query(modifySql); 
+  
+  // let result = api.query("SELECT * FROM aws_ec2.start_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  
+  return result;                       
+
+  // https://console.staging.transposit.com/t/yoko/growth_bot/code/op/z_deprecated_check_status_and_post for await
   return {
     success: true
   };
