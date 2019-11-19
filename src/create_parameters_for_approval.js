0af6cfd61af4dcb176440a0d1c11b95f5557adc1
Dan Moore <mooreds@transposit.com>
2019-11-19 22:08:14 +0000

    logging

--- src/create_parameters_for_approval.js
+++ src/create_parameters_for_approval.js
@@ -14,6 +14,7 @@
   const valueStr = JSON.stringify(value);
   
   console.log(user);
+  console.log(params.approvalUser);
   parameters.link_names = true;
   parameters.blocks = [{
       "type": "section",
