b9b94799eee64fed863df7cc3b0254748feb5ad9
Dan Moore <mooreds@transposit.com>
2019-11-19 23:58:36 +0000

    check every 30 seconds

---
+++ src/check_if_ec2_shutdown_operation_completed.js
@@ -0,0 +1,21 @@
+(params) => {
+  const moment = require('moment-timezone-with-data.js');
+
+  let result = api.query("SELECT instancesSet.item.instanceState.name FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  if (result.name == "stopped") {
+    console.log("stopped");
+    // call another operation??
+    return;
+  } 
+  
+  
+  const in30seconds = moment().add(30, "seconds").format();
+  task.create("this.check_if_ec2_shutdown_operation_completed", {instanceId: params.instanceId}).runOnce(in30seconds);
+  console.log("running in one min");
+  return;
+}
+
+/*
+ * For sample code and reference material, visit
+ * https://www.transposit.com/docs/references/js-operations
+ */
--- src/resize_ec2_instance.js
+++ src/resize_ec2_instance.js
@@ -1,7 +1,8 @@
 (params) => {
   
-  // let result = api.query("SELECT * FROM aws_ec2.stop_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
-  // let result = api.query("SELECT * FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  //let result = api.query("SELECT * FROM aws_ec2.stop_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
+  
+  let result = api.query("SELECT instancesSet.item.instanceState.name FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '"+params.instanceId+"' ] })");
   // const modifySql = "SELECT * FROM aws_ec2.modify_instance_attribute WHERE $body=(SELECT { 'Attribute' : 'instanceType', 'InstanceId' : '"+
   //                        params.instanceId+"', 'InstanceType' : { 'Value' : '"+params.newSize+"'} })"
   // console.log(modifySql);
