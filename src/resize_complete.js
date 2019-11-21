7f279e9bf2232c0d2ee9be3577330c5fb5de3dc3
Dan Moore <mooreds@transposit.com>
2019-11-21 16:33:02 +0000

    correct instanceId location

--- src/resize_complete.js
+++ src/resize_complete.js
@@ -3,10 +3,10 @@
   let result = api.query("SELECT instancesSet.item.instanceState.name FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '" + params.instanceId + "' ] })")[0];
 
   if (result.name == "running") {
-    const text = "Resizing instance " + approveObj.instanceId + " succeeded and it's up and running again.";
+    const text = "Resizing instance " + params.instanceId + " succeeded and it's up and running again.";
     
   } else {
-    const text = "Resizing instance " + approveObj.instanceId + " failed. Please investigate.";
+    const text = "Resizing instance " + params.instanceId + " failed. Please investigate.";
   }
   return api.run("this.post_text_only_message", {
       text: text,
