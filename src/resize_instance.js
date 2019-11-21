ad6d6c8258f3fa2cd0cc1f5fa182a4dc072ab608
Dan Moore <mooreds@transposit.com>
2019-11-21 00:18:16 +0000

    logging

--- src/resize_instance.js
+++ src/resize_instance.js
@@ -7,7 +7,10 @@
   const modifySql = "SELECT * FROM aws_ec2.modify_instance_attribute WHERE $body=(SELECT { 'Attribute' : 'instanceType', 'InstanceId' : '" +
     params.instanceId + "', 'InstanceType' : { 'Value' : '" + params.newSize + "'} })"
   console.log(modifySql);
+  console.log("aaaa");
   let result = api.query(modifySql);
+  console.log(result);
+  console.log("aaaa2");
   if (result.success) {
     let startResult = api.query("SELECT * FROM aws_ec2.start_instances WHERE $body=(SELECT { 'InstanceId' : [ '" + params.instanceId + "' ] })");
 
