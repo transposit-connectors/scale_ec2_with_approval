262f75f18f22d74ba9703ff6aaf1a86b47fce242
Dan Moore <mooreds@transposit.com>
2019-11-21 16:35:50 +0000

    scope text correctly

--- src/resize_complete.js
+++ src/resize_complete.js
@@ -2,11 +2,9 @@
 
   let result = api.query("SELECT instancesSet.item.instanceState.name FROM aws_ec2.describe_instances WHERE $body=(SELECT { 'InstanceId' : [ '" + params.instanceId + "' ] })")[0];
 
+  let text = "Resizing instance " + params.instanceId + " failed. Please investigate.";
   if (result.name == "running") {
-    const text = "Resizing instance " + params.instanceId + " succeeded and it's up and running again.";
-    
-  } else {
-    const text = "Resizing instance " + params.instanceId + " failed. Please investigate.";
+    text = "Resizing instance " + params.instanceId + " succeeded and it's up and running again.";
   }
   return api.run("this.post_text_only_message", {
       text: text,
